ee61cefa0042597ca68a78a1de1923fa55ac7ae4
Patrick Khouri <patrick.darya@gmail.com>
2024-08-19 17:55:52 -0300

    removed test code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header" class="text-blue-900">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
