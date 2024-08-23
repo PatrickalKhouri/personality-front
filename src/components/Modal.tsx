9b984d5cd6f6456598c7b08a9f1fa12cab096905
Patrick Khouri <patrick.darya@gmail.com>
2024-08-23 15:41:00 -0300

    added a fade in and fade out effect

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,27 +1,32 @@
 import React, { useState } from 'react';
-import { Questions } from '../App'
+import { Questions } from '../App';
 
 interface ModalProps {
   closeModal: () => void;
-  questions: Questions[]
+  questions: Questions[];
 }
 
 const Modal: React.FC<ModalProps> = ({ closeModal, questions }) => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
   const [answers, setAnswers] = useState<{ [key: number]: number }>({});
-  const [totalScore, setTotalScore] = useState<number>(0)
+  const [totalScore, setTotalScore] = useState<number>(0);
+  const [fade, setFade] = useState<string>('fade-in');
 
   const handleAnswerSelect = (questionId: number, answerId: number, answerScore: number) => {
-    setAnswers(prevAnswers => ({
-      ...prevAnswers,
-      [questionId]: answerId,
-    }));
-    setTotalScore(totalScore + answerScore)
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-    } else {
-      console.log('Quiz completed with answers:', totalScore);
-    }
+    setFade('fade-out');
+    setTimeout(() => {
+      setAnswers(prevAnswers => ({
+        ...prevAnswers,
+        [questionId]: answerId,
+      }));
+      setTotalScore(totalScore + answerScore);
+      if (currentQuestionIndex < questions.length - 1) {
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+      } else {
+        console.log('Quiz completed with answers:', totalScore);
+      }
+      setFade('fade-in');
+    }, 500); // 500ms matches the duration of the animation
   };
 
   const currentQuestion = questions[currentQuestionIndex];
@@ -29,22 +34,26 @@ const Modal: React.FC<ModalProps> = ({ closeModal, questions }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-[#FEFEE3] p-6 rounded-lg shadow-lg w-full max-w-md md:max-w-2xl">
-        <h2 className="text-2xl mb-4">{currentQuestion.question}</h2>
-        <div>
-          {currentQuestion.answers.map((answer: any) => (
-            <button
-              key={answer.id}
-              onClick={() => handleAnswerSelect(currentQuestion.id, answer.id, answer.score)}
-              className="block w-full text-left mb-2 px-4 py-2 bg-[#D68C45] text-[#FEFEE3] rounded"
-            >
-              {answer.answer}
-            </button>
-          ))}
+        <div className={`transition-opacity duration-500 ${fade}`}>
+          <h2 className="text-2xl mb-4">
+            {currentQuestionIndex + 1}. {currentQuestion.question}
+          </h2>
+          <div>
+            {currentQuestion.answers.map((answer: any) => (
+              <button
+                key={answer.id}
+                onClick={() => handleAnswerSelect(currentQuestion.id, answer.id, answer.score)}
+                className="block w-full text-left mb-2 px-4 py-2 bg-[#D68C45] text-[#FEFEE3] rounded"
+              >
+                {answer.answer}
+              </button>
+            ))}
+          </div>
         </div>
         <button 
           onClick={closeModal} 
           className="mt-4 px-4 py-2 bg-[#D68C45] text-[#FEFEE3] rounded">
-          Close Modal
+          Close
         </button>
       </div>
     </div>
