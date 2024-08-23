2c1b9c7811b0651d20564028d508cc969e5d9709
Patrick Khouri <patrick.darya@gmail.com>
2024-08-23 09:27:32 -0300

    handles the total score

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,19 +7,20 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ closeModal, questions }) => {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
   const [answers, setAnswers] = useState<{ [key: number]: number }>({});
+  const [totalScore, setTotalScore] = useState<number>(0)
 
-  const handleAnswerSelect = (questionId: number, answerId: number) => {
+  const handleAnswerSelect = (questionId: number, answerId: number, answerScore: number) => {
     setAnswers(prevAnswers => ({
       ...prevAnswers,
       [questionId]: answerId,
     }));
-
+    setTotalScore(totalScore + answerScore)
     if (currentQuestionIndex < questions.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     } else {
-      console.log('Quiz completed with answers:', answers);
+      console.log('Quiz completed with answers:', totalScore);
     }
   };
 
@@ -33,7 +34,7 @@ const Modal: React.FC<ModalProps> = ({ closeModal, questions }) => {
           {currentQuestion.answers.map((answer: any) => (
             <button
               key={answer.id}
-              onClick={() => handleAnswerSelect(currentQuestion.id, answer.id)}
+              onClick={() => handleAnswerSelect(currentQuestion.id, answer.id, answer.score)}
               className="block w-full text-left mb-2 px-4 py-2 bg-[#D68C45] text-[#FEFEE3] rounded"
             >
               {answer.answer}
